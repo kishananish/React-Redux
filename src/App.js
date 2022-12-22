73d3368dd6469afcac76a6b5fa1ffbf365e90b61
kishananish <kishan.kumar@neosoftmail.com>
2022-12-22 19:02:58 +0530

    React redux increment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { incNumber, decNumber, addFiveNumber } from "./actions";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const myState = useSelector((state) => state.changeTheNumber);
+  const dispatch = useDispatch();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div classname="container">
+        <h1>Increment/Decrement counter</h1>
+        <h4>using React and Redux</h4>
+        <div classname="quantity">
+          <button
+            classname="quantity__minus"
+            title="Decrement"
+            onClick={() => dispatch(decNumber())}
+          >
+            <span>-</span>
+          </button>
+          <input
+            name="quantity"
+            type="text"
+            classname="quantity__input"
+            value={myState}
+          />
+          <button
+            classname="quantity__plus"
+            title="Increment"
+            onClick={() => dispatch(incNumber(3))}
+          >
+            <span>5</span>
+          </button>
+
+          <button
+            classname="quantity__plus"
+            title="Addfivenumber"
+            onClick={() => dispatch(addFiveNumber(5))}
+          >
+            <span>+5</span>
+          </button>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
